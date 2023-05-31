78f53d949d3d606a3f5831957ad7f3b883d3de3b
daniel <dmaixner93@gmail.com>
2023-05-30 21:09:56 -0500

    Update hero styles

--- src/pages/main/Introduction.js
+++ src/pages/main/Introduction.js
@@ -3,12 +3,11 @@ import Spacer from '../../components/Spacer';
 
 const Introduction = () => {
   return (
-    <div className="w-full text-gray-700 dark:text-gray-300 space-y-4 text-left">
-      <h3 className="text-base font-medium text-blue-500 dark:text-blue-300 pl-1">Hi, my name is</h3>
-      <h1 className="text-4xl text-gray-600 dark:text-gray-100 font-semibold">Daniel Maixner</h1>
-      <p className="text-5xl">I like to build and take apart things.</p>
+    <div className="w-full text-gray-700 dark:text-gray-300 space-y-2 lg:space-y-4 text-center lg:text-left">
+      <h3 className="text-lg font-medium text-blue-500 dark:text-blue-300 lg:pl-1">Hi, my name is</h3>
+      <h1 className="text-5xl text-gray-600 dark:text-gray-100 font-semibold">Daniel Maixner</h1>
       <p className="leading-6 max-w-xl">
-        I am a self-taught software engineer and automotive enthusiast. I currently work at World Wide Technology as a software developer for the Advanced Technology Center.
+        I am a self-taught software engineer and automotive enthusiast.
       </p>
       <Spacer height={4} />
       <Button />
